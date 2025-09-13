74d89ee4845e6ef64eee60cb303fa53bf27014b9
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-13 13:05:41 +0000

    Learings on the styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
 export default function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <H1>Hello world</H1>
+    </div>
+  );
 }
