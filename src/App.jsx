843acb9543af5493ff92d970199d48ab4f0ab9c6
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-18 16:55:28 +0000

    Adjustment to the input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/input";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -12,6 +14,8 @@ export default function App() {
       <GlobalStyles />
       <div>
         <H1>Hello world</H1>
+        <Button>New</Button>
+        <Input placeholder="type here.." />
       </div>
     </>
   );
