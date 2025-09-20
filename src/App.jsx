7cdc0fa4b7c2efc37f2f774266bdfab4c443d7c1
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-20 19:57:38 +0000

    Work on the header styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import Heading from "./ui/Heading";
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
       <div>
-        <H1>Hello world</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
         <Button>New</Button>
         <Input placeholder="type here.." />
       </div>
