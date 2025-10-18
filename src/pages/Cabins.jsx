910edd903a7166d3698777d8a1c771dfac65bacc
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-18 22:55:16 +0000

    Fetch the cabins data from supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,17 +2,23 @@
 // import AddCabin from "../features/cabins/AddCabin";
 // import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  // useEffect(function () {
-  //   getCabins().then((data) => console.log(data));
-  // }, []);
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
+        <img
+          src="https://vhqjiqyiqapbmtykxuun.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+          alt=""
+        />
         {/* <CabinTableOperations /> */}
       </Row>
       {/* 
