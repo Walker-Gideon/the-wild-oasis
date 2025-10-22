765c49231e264376b9b32170e22f80c44ebeb7e5
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-22 23:53:35 +0000

    Added the helper function and update in the cabinrow component

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,6 +1,19 @@
-// import styled from "styled-components";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { formatCurrency } from "/src/utils/helpers";
 
-/*
+const TableRow = styled.div`
+  display: grid;
+  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+  column-gap: 2.4rem;
+  align-items: center;
+  padding: 1.4rem 2.4rem;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+`;
+
 const Img = styled.img`
   display: block;
   width: 6.4rem;
@@ -27,8 +40,28 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
-*/
 
-export default function CabinRow() {
-  return <div>Row</div>;
+export default function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
 }
+
+CabinRow.propTypes = {
+  cabin: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    maxCapacity: PropTypes.number.isRequired,
+    regularPrice: PropTypes.number.isRequired,
+    discount: PropTypes.number,
+    image: PropTypes.string,
+  }).isRequired,
+};
