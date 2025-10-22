bd79167650530bd5e5a313f45e004c52d78f97d6
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-22 22:25:25 +0000

    Adjustment to the cabins section

--- src/features/cabins/Cabins.jsx
+++ src/features/cabins/Cabins.jsx
@@ -1,25 +1,12 @@
-// import CabinTable from "../features/cabins/CabinTable";
-// import AddCabin from "../features/cabins/AddCabin";
-// import CabinTableOperations from "../features/cabins/CabinTableOperations";
-
 import Heading from "../../ui/Heading";
 import Row from "../../ui/Row";
-import { getCabins } from "../../services/apiCabins";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-
-        {/* <CabinTableOperations /> */}
       </Row>
-      {/* 
-
-      <Row>
-        <CabinTable />
-        <AddCabin />
-      </Row> */}
     </>
   );
 }
