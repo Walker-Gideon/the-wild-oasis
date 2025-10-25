00c0941b3038e13d09f8cbe379a1d6466ea3d7a5
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-25 11:57:31 +0000

    Adjust the form content in the CreatCabinFom component

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,12 +1,42 @@
 import FormRow from "/src/ui/FormRow";
 import Input from "/src/ui/input";
 import Form from "/src/ui/Form";
+import Textarea from "../../ui/Textarea";
+import FileInput from "../../ui/FileInput";
+import Button from "../../ui/Button";
 
 export default function CreateCabinForm() {
   return (
     <Form>
       <FormRow label="Cabin name">
         <Input type="text" id="name" />
+      </FormRow>
+
+      <FormRow label="Maximum Capacity">
+        <Input type="number" id="maxCapacity" />
+      </FormRow>
+
+      <FormRow label="Regular Price">
+        <Input type="number" id="regularPrice" />
+      </FormRow>
+
+      <FormRow label="Discount">
+        <Input type="number" id="discount" />
+      </FormRow>
+
+      <FormRow label="Description">
+        <Textarea type="number" id="description" />
+      </FormRow>
+
+      <FormRow label="Cabin Photo">
+        <FileInput id="image" type="file" />
+      </FormRow>
+
+      <FormRow>
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button>Create cabin</Button>
       </FormRow>
     </Form>
   );
