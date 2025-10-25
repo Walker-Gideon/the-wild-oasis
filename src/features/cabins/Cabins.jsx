2d89fa9a5904c49b52c42cb9b01dc733dd1741fd
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-25 12:32:46 +0000

    Added the CreateCabinForm in the Cabin component

--- src/features/cabins/Cabins.jsx
+++ src/features/cabins/Cabins.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
+import CreateCabinForm from "./components/CreateCabinForm";
 import CabinTable from "./components/CabinTable";
 import Heading from "/src/ui/Heading";
+import Button from "/src/ui/Button";
 import Row from "/src/ui/Row";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false)
+
   return (
     <>
       <Row type="horizontal">
@@ -11,6 +17,9 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm(show => !show)}>Add new cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
